c69065ff5fc1a98222a143fa61673d22008907f0
CGMajoor <cgmajoor@gmail.com>
2022-02-11 00:31:33 +0100

    add more rules to swift lint

--- Arty/Modules/Library/LibraryViewController.swift
+++ Arty/Modules/Library/LibraryViewController.swift
@@ -13,6 +13,10 @@ class LibraryViewController: UIViewController {
     // MARK: - Dependencies
 
     @Injected private var viewModel: LibraryViewModel
+
+    // MARK: - UI
+
+    private var activityIndicatorView = UIActivityIndicatorView(style: .large)
     
     // MARK: - Lifecycle
 
@@ -21,14 +25,34 @@ class LibraryViewController: UIViewController {
         navigationItem.title = "Collection"
 
         setup()
+
+        getCollection()
     }
-    
+
     // MARK: - Private
 
     private func setup() {
         view.backgroundColor = .black
-        
-        viewModel.fetchCollection { _, _  in
+
+        [activityIndicatorView].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        setupLayoutConstraints()
+    }
+
+    private func setupLayoutConstraints() {
+        activityIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    private func getCollection() {
+        activityIndicatorView.startAnimating()
+
+        viewModel.fetchCollection { [weak self] _, _  in
+            guard let self = self else { return }
+            self.activityIndicatorView.stopAnimating()
             print("done")
         }
     }
